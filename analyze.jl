3acf413221f8bc1f17547c99076d84ff9c047faa
Dmitry Vyal <akamaus@gmail.com>
2014-12-20 15:19:10 +0300

    Some preliminary statistical analysis

---
+++ analyze.jl
@@ -0,0 +1,78 @@
+using HDF5
+
+function read_electrodes()
+    data_path = "/home/dvyal/Shared/2014_03_20_0001.h5"
+    df = h5open(data_path,"r")
+    df["Data/Recording_0/AnalogStream/Stream_0/ChannelData"]
+end
+
+function detect_spikes(ch; prec = 2)
+    mean_p = mean(ch)
+    noise = std(ch) * prec
+
+    spikes = (Int32,Int32)[]
+
+    for i=1:length(ch)
+        v = ch[i]
+        if (abs(v - mean_p) > noise)
+            push!(spikes, (i, v))
+        end
+    end
+
+    spikes
+end
+
+function analize()
+    info("Reading electrodes")
+    channels = read_electrodes()
+
+    for i=1:size(channels,2)
+        ch = channels[:,i] :: Array{Int32,2}
+        ch = reshape(ch, length(ch))
+        spikes = detect_spikes(ch)
+        @printf("channel %u; spikes = %u", i, length(spikes))
+    end
+end
+
+function plot_stats(xs; slice=20000)
+    slices = reshape(xs, div(length(xs),slice), slice)
+    s_mean = mean(slices,2)
+    s_std = std(slices,2)
+    plot(layer(x=1:length(s_mean), y = s_mean, Geom.line),
+         layer(x=1:length(s_std), y = s_std, Geom.line))
+end
+
+function anomaly_filter(xs, x_mean, x_deviation)
+    sieved = (Int64,Int32)[]
+    for x in xs
+        p,v = x
+        if (abs(v - x_mean) > x_deviation)
+            push!(sieved, x)
+        end
+    end
+    sieved
+end
+
+function anomaly_hist(ch)
+    mean_p = mean(ch)
+    noise = std(ch)
+    bins = Int64[]
+
+    n=0
+    pairs = map(ch) do x
+        global n
+        n += 1
+        (n, x)
+    end
+
+    k=1
+    while length(pairs) > 0
+        push!(bins,length(pairs))
+        @printf("len_pairs %u\n", length(pairs))
+        pairs = anomaly_filter(pairs, mean_p, noise*k)
+        k += 1
+    end
+
+    plot(x=1:k-1, y=bins)
+    bins
+end
